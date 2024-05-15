a4102919d79e5eaeed8f8112ff8f7cf618131937
ohseungwon56 <155320520+ohseungwon56@users.noreply.github.com>
2024-05-15 20:38:02 +0900

    Title: 특정 조건 두 정수 비교, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240515/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -0,0 +1,13 @@
+n=input()
+arr=n.split()
+a=int(arr[0])
+b=int(arr[1])
+if a<b:
+    print(1, end=" ")
+else:
+    print(0, end=" ")
+
+if a==b:
+    print(1, end=" ")
+else:
+    print(0, end=" ")
