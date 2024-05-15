894bf1eb030c697e8f4aeaa2150a107514806cb0
ohseungwon56 <155320520+ohseungwon56@users.noreply.github.com>
2024-05-15 20:42:22 +0900

    Title: 비교 연산, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240515/비교 연산/comparison-operator.py
@@ -0,0 +1,28 @@
+n=input()
+arr=n.split()
+a=int(arr[0])
+b=int(arr[1])
+if a>=b:
+    print(1)
+else:
+    print(0)
+if a>b:
+    print(1)
+else:
+    print(0)
+if b>=a:
+    print(1)
+else:
+    print(0)
+if b>a:
+    print(1)
+else:
+    print(0)
+if a==b:
+    print(1)
+else:
+    print(0)
+if a!=b:
+    print(1)
+else:
+    print(0)
