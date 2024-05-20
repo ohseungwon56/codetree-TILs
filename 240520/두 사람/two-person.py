343b1b4f2e1b6ad7b629aed283a4d8ff437473e1
ohseungwon56 <155320520+ohseungwon56@users.noreply.github.com>
2024-05-20 20:56:39 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240520/두 사람/two-person.py
@@ -0,0 +1,11 @@
+n=input()
+m=input()
+a=n.split()
+b=m.split()
+a_age, a_gender = int(a[0]), a[1]
+b_age, b_gender = int(b[0]), b[1]
+
+if (a_age >= 19 or b >= 19) and (a_gender=='M' or b_gender=='M'):
+    print(1)
+else:
+    print(0)
