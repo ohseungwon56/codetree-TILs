12e217cc365773d180ad723a693913f4edce6077
ohseungwon56 <155320520+ohseungwon56@users.noreply.github.com>
2024-05-20 20:58:41 +0900

    Title: 두 사람, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240520/두 사람/two-person.py
+++ 240520/두 사람/two-person.py
@@ -5,7 +5,7 @@ b=m.split()
 a_age, a_gender = int(a[0]), a[1]
 b_age, b_gender = int(b[0]), b[1]
 
-if (a_age >= 19 or b >= 19) and (a_gender=='M' or b_gender=='M'):
+if (a_age >= 19 or b_age >= 19) and (a_gender=='M' or b_gender=='M'):
     print(1)
 else:
     print(0)
