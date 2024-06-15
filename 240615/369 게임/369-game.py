64611f56a8e19fc4eff20409790b791bc28bb063
ohseungwon56 <155320520+ohseungwon56@users.noreply.github.com>
2024-06-15 09:40:20 +0900

    Title: 369 게임, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240615/369 게임/369-game.py
+++ 240615/369 게임/369-game.py
@@ -3,6 +3,6 @@ for i in range(1, n+1):
     if i%3 == 0:
         print(0, end=' ')
     elif i%3 == 1:
-        print(1, end=' ')
+        print(i, end=' ')
     else:
-        print(2, end=' ')
+        print(i, end=' ')
